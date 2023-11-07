89a1f620d021aa781a65dc638a4a1e64dfee269c
N1GH7M4R3 <yordanbb98@gmail.com>
2023-11-07 15:35:31 -0500

    exporting types

---
+++ src/utils/types.js
@@ -0,0 +1,45 @@
+import DashboardIcon from "../assets/MyDashboard/icon-buildercolumn.svg";
+import BulletIcon from "../assets/MyDashboard/icon-builderbulletlist.svg";
+import HeadingIcon from "../assets/MyDashboard/icon-builderheading.svg";
+import ImageIcon from "../assets/MyDashboard/icon-builderimage.svg";
+import LinkIcon from "../assets/MyDashboard/icon-builderlink.svg";
+import NumberedListIcon from "../assets/MyDashboard/icon-buildernumberedlist.svg";
+import TextIcon from "../assets/MyDashboard/icon-buildertext.svg";
+
+export const itemsTypes = [
+    {
+        id:1,
+        title: "Analysis Board",
+        icon: DashboardIcon,
+    },
+    {
+        id:2,
+        title: "Bullet List",
+        icon: BulletIcon,
+    },
+    {
+        id:3,
+        title: "Heading",
+        icon: HeadingIcon,
+    },
+    {
+        id:4,
+        title: "Image",
+        icon: ImageIcon,
+    },
+    {
+        id:5,
+        title: "HyperLink",
+        icon: LinkIcon,
+    },
+    {
+        id:6,
+        title: "Numbered List",
+        icon: NumberedListIcon,
+    },
+    {
+        id:7,
+        title: "Texto",
+        icon: TextIcon,
+    },
+];
