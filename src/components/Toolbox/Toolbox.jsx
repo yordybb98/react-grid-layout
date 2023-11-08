ec596ee9481ca2338e5051609cb12f47fbfc0282
N1GH7M4R3 <yordanbb98@gmail.com>
2023-11-08 09:50:02 -0500

    Inserting Functions to ToolBox Items

--- src/components/Toolbox/Toolbox.jsx
+++ src/components/Toolbox/Toolbox.jsx
@@ -2,24 +2,37 @@ import React, { useState } from "react";
 import Item from "../Item/Item";
 import { itemsTypes } from "../../utils/types";
 
-function Toolbox() {
+function Toolbox({ addItem }) {
     const [list, setList] = useState(itemsTypes);
+
     return (
-        <div style={{ display: "flex", gap: 5, marginBottom: "10px" }}>
+        <div style={{ display: "flex", gap: 30 }}>
             {list.map((item) => (
                 <div
                     key={item.id}
                     style={{
-                        width: 75,
-                        height: 75,
+                        width: 80,
+                        height: 80,
                         boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 20px",
                         backgroundColor: "#ffffff73",
                         display: "flex",
+                        flexDirection: "column",
                         justifyContent: "center",
                         alignItems: "center",
                         textAlign: "center",
+                        gap: 10,
+                        cursor: "pointer",
+                        padding: 5,
+                    }}
+                    onClick={() => {
+                        addItem(item.id);
+                        /* To delete the added item from Toolbox */
+                        /* setList((prevList) => {
+                            return prevList.filter((itemList) => itemList.id !== item.id);
+                        }); */
                     }}>
                     {item.title}
+                    <img src={item.icon} alt="" width={20} height={20} />
                 </div>
             ))}
         </div>
